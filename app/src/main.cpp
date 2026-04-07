7796134781aa06fa0b0a972d53a9fa15a5249409
MIDORIINU <diegorluna@gmail.com>
2026-04-07 10:02:48 +0000

    Completed task 3.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,33 +1,43 @@
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/pwm.h>
 #include <zephyr/logging/log.h>
 
-#define SLEEP_TIME_MS 333
 
-
-/* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(led0)
-
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+/* Get the PWM device from the devicetree alias */
+static const struct pwm_dt_spec led_pwm = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
-    bool led_state = true;
+    bool led_state = false;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
+    uint32_t led_brightness_step = led_pwm.period / 100; // 1%
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    if (!pwm_is_ready_dt(&led_pwm))
+        return 0;
 
-    while (1) {
-        // Toggle the LED.
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;             
-
+    while (1)
+    {
         led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_BLINK_SLEEP_MS);
+
+	    uint32_t duty_cycle = (CONFIG_LED_BRIGHTNESS * led_brightness_step);
+
+	    if (pwm_set_pulse_dt(&led_pwm, led_state ? duty_cycle : 0) < 0)
+		    return 0;
+
+        if (CONFIG_LED_DEBUG)
+            LOG_INF("LED state: %s (%d%% duty cycle [%d ns/%d ns])",
+                    led_state ? "ON" : "OFF",
+                    led_state ? CONFIG_LED_BRIGHTNESS : 0,
+                    led_state ? duty_cycle : 0,
+                    led_pwm.period);
+
+	    k_msleep(CONFIG_BLINK_SLEEP_MS);
     }
+
     return 0;
 
 }
